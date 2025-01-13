479705eb8d081521a475cca241c49a65132f0a7f
Andile Mnukwa <andile.reeman.com>
2025-01-13 15:30:44 +0200

    first

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) =>{
+    res.send("Welcome our chat app APIs..");
+});
+
+const port = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connection established"))
+    .catch((error) => console.log("MongoDB connection failed: ", error.message));
